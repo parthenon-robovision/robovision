8b0e413d0025ea53a3995b15b45ee955f1c03872
Tyler Spaeth <tspaeth@parthenonsoftware.com>
2017-03-23 09:18:17 -0700

    Fix test server script.

--- test_server.sh
+++ test_server.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ssh -i deployment/keys/vagrant.key -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p 2222 -L 5000:127.0.0.1:5000 vagrant@localhost -C 'sudo -u imagery bash -c "killall flask; cd /srv/www && source Envs/imagery/bin/activate && cd imagery/app && GOOGLE_APPLICATION_CREDENTIALS=/srv/credentials/gcloud.json FLASK_DEBUG=1 FLASK_APP=app.py flask run"'
+ssh -i deployment/keys/vagrant.key -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p 2222 -L 5000:127.0.0.1:5000 vagrant@localhost -C 'sudo -u imagery bash -c "killall python; cd /srv/www && source Envs/imagery/bin/activate && cd imagery/app && GOOGLE_APPLICATION_CREDENTIALS=/srv/imagery_credentials/gcloud.json FLASK_DEBUG=1 FLASK_APP=app.py flask run"'
