738801faecc84851f7db9a8d60260b2478256555
Tyler Spaeth <tspaeth@parthenonsoftware.com>
2017-03-29 09:31:59 -0700

    vault_pass.py displays an error message if IMAGERY_VAULT_PASSWORD isn't set in the environment.

--- vault_pass.py
+++ vault_pass.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python
 import os
+import sys
 
-print os.environ['IMAGERY_VAULT_PASSWORD']
+if 'IMAGERY_VAULT_PASSWORD' in os.environ:
+    print os.environ['IMAGERY_VAULT_PASSWORD']
+
+sys.stderr.write('***********\n')
+sys.stderr.write('***Error*** Please set the environment variable IMAGERY_VAULT_PASSWORD\n')
+sys.stderr.write('***********\n')
+exit(1)
