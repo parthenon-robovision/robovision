a1f895498af97a965f47f6457c0501c6a56c91ad
Tyler Spaeth <tspaeth@parthenonsoftware.com>
2017-04-11 15:14:27 -0700

    Cleaned up and commented verbatim.py.

--- app/verbatim.py
+++ app/verbatim.py
@@ -1,37 +1,74 @@
-from collections import namedtuple
-import json
-import os
-import pty
-import re
-from subprocess import check_output, Popen, PIPE
-import sys
+''' Decompose a sentence into a list of phrases with each phrase being about
+a specific subject in the sentence.
 
-DESCRIPTORS = ('acomp', 'amod', 'appos', 'det',
-    'predet', 'mwe', 'rcmod', 'nn', 'num', 'quantmod', 'cc', 'conj')
+Note: this was built to parse results from CloudSight and is aimed at simple
+descriptions of images such as 'A man playing chess in a Panama hat.' Anything
+that follows 'It was the best of times, it was the worst of times, ...' will
+probably fail.
+'''
+
+from collections import namedtuple
+import os
+from subprocess import PIPE, Popen
+
+DESCRIPTORS = ('acomp', 'amod', 'appos', 'det', 'predet', 'mwe', 'rcmod', 'nn',
+               'num', 'quantmod', 'cc', 'conj')
 SUBJECTS = ('dobj', 'iobj', 'pobj', 'obj', 'subj', 'nsubj', 'csubj')
 
-Node = namedtuple('Node', ['value', 'children'])
-Term = namedtuple('Term', ['id', 'word', 'pos', 'parent', 'function'])
-Subject = namedtuple('Subject', ['noun', 'descriptors'])
+class Node(namedtuple('Node', ['term', 'children'])):
+    '''The building block for dependency graph construction.
+
+    term -- a Term from the CoNLL representation.
+    children -- all the Nodes/Terms that depend on this Node/Term.
+    '''
+    pass
+
+
+class Term(namedtuple('Term', ['id', 'word', 'pos', 'parent', 'function'])):
+    '''The properties of a word in a sentence we care about.
+
+    id -- the index of the word in the sentence.
+    word -- the word in string form.
+    pos -- the word's part of speech in the sentence.
+    parent -- the id of the term this term depends on.
+    function -- the function of the word in the sentence.
+    '''
+    pass
+
+
+class Subject(namedtuple('Subject', ['noun', 'descriptors'])):
+    '''A thing and the words that describe it. Ideally, the descriptors make
+    sense when comma separated and placed in front of noun.
+
+    noun -- a string
+    descriptors -- a list of strings
+    '''
+    pass
 
 def _line_to_terms(line):
     '''Takes a valid, tab-separated CoNLL line and parses it into a Term.
 
     See https://nlp.stanford.edu/software/dependencies_manual.pdf
     '''
-    return Term(id=int(line[0]), word=line[1], pos=line[4].lower(), parent=int(line[6]), function=line[7])
+    return Term(
+        id=int(line[0]),
+        word=line[1],
+        pos=line[4].lower(),
+        parent=int(line[6]),
+        function=line[7]
+    )
 
 def _terms_to_tree(terms):
-    ''' Convert a list of Terms to a tree format.
+    ''' Convert a list of Terms to a tree structure.
 
     See Node above.
     '''
     def _make_tree(root):
         children = [term for term in terms if term.parent == root.id]
         if len(children) == 0:
-            return Node(value=root, children=None)
+            return Node(term=root, children=None)
         children = [_make_tree(child) for child in children]
-        return Node(value=root, children=children)
+        return Node(term=root, children=children)
 
     for term in terms:
         if term.parent == 0:
@@ -40,12 +77,6 @@ def _terms_to_tree(terms):
 
     return _make_tree(root)
 
-def print_tree(tree, lead):
-    if tree.children is None:
-        return
-    for child in tree.children:
-        print_tree(child, '{}\t'.format(lead))
-
 def _find_subjects(tree_root_node):
     '''Find all the subjects in the given tree along with their descriptors.'''
     def _crawl_descriptors(children, subject):
@@ -53,22 +84,22 @@ def _find_subjects(tree_root_node):
             return
         skip = False
         for i in xrange(len(children)):
-            if children[i].value.function in DESCRIPTORS:
-                if children[i].value.function == 'cc' and children[i + 1].value.pos == 'nn':
+            if children[i].term.function in DESCRIPTORS:
+                if children[i].term.function == 'cc' and children[i + 1].term.pos == 'nn':
                     skip = True
                     continue
-                if children[i].value.pos != 'dt' and not skip:
-                    subject.descriptors.append(children[i].value)
+                if children[i].term.pos != 'dt' and not skip:
+                    subject.descriptors.append(children[i].term)
                 else:
                     skip = False
                 if children[i].children is not None:
                     _crawl_descriptors(children[i].children, subject)
 
     def _crawl_tree(node, done):
-        if (node.value.function in SUBJECTS
-            or node.value.function == 'ROOT' and node.value.pos == 'nn'
-            or node.value.function == 'conj' and node.value.pos == 'nn'):
-            subject = Subject(noun=node.value, descriptors=[])
+        if (node.term.function in SUBJECTS
+                or node.term.function == 'ROOT' and node.term.pos == 'nn'
+                or node.term.function == 'conj' and node.term.pos == 'nn'):
+            subject = Subject(noun=node.term, descriptors=[])
             _crawl_descriptors(node.children, subject)
             done.append(subject)
 
@@ -86,25 +117,30 @@ def _find_subjects(tree_root_node):
     return done
 
 def list_subjects(phrase):
-    name = check_output('whoami', shell=True)
-    safe_phrase = str(phrase).replace("'", '')
+    '''Pull the subjects and the words that describe them out of the given
+    phrase.
+
+    phrase -- Any well formed sentence.
+
+    returns -- a list of phrases, one for each subject.
+    '''
     try:
-        master_fd, slave_fd = pty.openpty()
+        FNULL = open(os.devnull, 'w')
         parser = Popen(
             ['/usr/bin/docker', 'run', '--rm', '-i', 'local/syntaxnet-docker', 'syntaxnet/demo.sh'],
             stdin=PIPE,
-            stdout=PIPE
+            stdout=PIPE,
+            stderr=FNULL
         )
     except Exception as e:
         print e
         raise
-    lines = parser.communicate(phrase)[0].split('\n')
-    print '\n'.join(lines)
-    table = []
-    for line in lines:
-        if re.match('^\d+.*$', line):
-            table.append(line.split('\t')[:-1])
+    lines = [line for line in parser.communicate(phrase)[0].split('\n') if line]
 
+    if __name__ == '__main__':
+        print '\n'.join(lines)
+
+    table = [line.split('\t')[:-1] for line in lines]
     terms = [_line_to_terms(line) for line in table]
     subjects = _find_subjects(_terms_to_tree(terms))
 
@@ -115,8 +151,12 @@ def list_subjects(phrase):
         if len(descriptors) > 0:
             subject = ' {}'.format(subject)
 
-        formatted_subjects.append('{}{}'.format(' '.join([descriptor.word.lower() for descriptor in descriptors]), subject))
+        formatted_subjects.append('{}{}'.format(
+            ', '.join([descriptor.word.lower() for descriptor in descriptors]),
+            subject
+        ))
 
     return formatted_subjects
 
-print list_subjects('Everything must be made of thin, strong paper without cuts or glue')
+if __name__ == '__main__':
+    print list_subjects('Everything must be made of thin, strong paper without cuts or glue')
