ca74911e31af056d4a9d8ad3a04cf8666c2ed684
Tyler Spaeth <tspaeth@parthenonsoftware.com>
2017-04-11 13:39:45 -0700

    WIP Refactor verbatim.py

--- app/verbatim.py
+++ app/verbatim.py
@@ -1,10 +1,10 @@
+from collections import namedtuple
+import json
+import os
+import pty
+import re
 from subprocess import check_output, Popen, PIPE
 import sys
-import pty
-import json
-import re
-from collections import namedtuple
-import os
 
 DESCRIPTORS = ('acomp', 'amod', 'appos', 'det',
     'predet', 'mwe', 'rcmod', 'nn', 'num', 'quantmod', 'cc', 'conj')
@@ -12,17 +12,33 @@ SUBJECTS = ('dobj', 'iobj', 'pobj', 'obj', 'subj', 'nsubj', 'csubj')
 
 Node = namedtuple('Node', ['value', 'children'])
 Term = namedtuple('Term', ['id', 'word', 'pos', 'parent', 'function'])
-Thing = namedtuple('Thing', ['noun', 'descriptors'])
+Subject = namedtuple('Subject', ['noun', 'descriptors'])
 
 def _line_to_terms(line):
+    '''Takes a valid, tab-separated CoNLL line and parses it into a Term.
+
+    See https://nlp.stanford.edu/software/dependencies_manual.pdf
+    '''
     return Term(id=int(line[0]), word=line[1], pos=line[4].lower(), parent=int(line[6]), function=line[7])
 
-def _make_tree(root, descriptions):
-    children = [desc for desc in descriptions if desc.parent == root.id]
-    if len(children) == 0:
-        return Node(value=root, children=None)
-    children = [_make_tree(child, descriptions) for child in children]
-    return Node(value=root, children=children)
+def _terms_to_tree(terms):
+    ''' Convert a list of Terms to a tree format.
+
+    See Node above.
+    '''
+    def _make_tree(root):
+        children = [term for term in terms if term.parent == root.id]
+        if len(children) == 0:
+            return Node(value=root, children=None)
+        children = [_make_tree(child) for child in children]
+        return Node(value=root, children=children)
+
+    for term in terms:
+        if term.parent == 0:
+            root = term
+            break
+
+    return _make_tree(root)
 
 def print_tree(tree, lead):
     if tree.children is None:
@@ -30,40 +46,44 @@ def print_tree(tree, lead):
     for child in tree.children:
         print_tree(child, '{}\t'.format(lead))
 
-def _crawl_descriptors(children, thing):
-    if children is None:
-        return
-    if len(children) >= 2 and children[-2].value.function == 'cc' and children[-1].value.pos == 'nn':
-        return
-    skip = False
-    for i in xrange(len(children)):
-        if children[i].value.function in DESCRIPTORS:
-            if children[i].value.function == 'cc' and children[i + 1].value.pos == 'nn':
-                skip = True
-                continue
-            if children[i].value.pos != 'dt' and not skip:
-                thing.descriptors.append(children[i].value)
-            else:
-                skip = False
-            if children[i].children is not None:
-                _crawl_descriptors(children[i].children, thing)
+def _find_subjects(tree_root_node):
+    '''Find all the subjects in the given tree along with their descriptors.'''
+    def _crawl_descriptors(children, subject):
+        if children is None:
+            return
+        skip = False
+        for i in xrange(len(children)):
+            if children[i].value.function in DESCRIPTORS:
+                if children[i].value.function == 'cc' and children[i + 1].value.pos == 'nn':
+                    skip = True
+                    continue
+                if children[i].value.pos != 'dt' and not skip:
+                    subject.descriptors.append(children[i].value)
+                else:
+                    skip = False
+                if children[i].children is not None:
+                    _crawl_descriptors(children[i].children, subject)
 
-def _crawl_tree(node, done):
-    if (node.value.function in SUBJECTS
-        or node.value.function == 'ROOT' and node.value.pos == 'nn'
-        or node.value.function == 'conj' and node.value.pos == 'nn'):
-        thing = Thing(noun=node.value, descriptors=[])
-        _crawl_descriptors(node.children, thing)
-        done.append(thing)
+    def _crawl_tree(node, done):
+        if (node.value.function in SUBJECTS
+            or node.value.function == 'ROOT' and node.value.pos == 'nn'
+            or node.value.function == 'conj' and node.value.pos == 'nn'):
+            subject = Subject(noun=node.value, descriptors=[])
+            _crawl_descriptors(node.children, subject)
+            done.append(subject)
 
-    if node.children is None:
-        return
+        if node.children is None:
+            return
 
-    for child in node.children:
-        _crawl_tree(
-            child,
-            done
-        )
+        for child in node.children:
+            _crawl_tree(
+                child,
+                done
+            )
+
+    done = []
+    _crawl_tree(tree_root_node, done)
+    return done
 
 def list_subjects(phrase):
     name = check_output('whoami', shell=True)
@@ -86,23 +106,17 @@ def list_subjects(phrase):
             table.append(line.split('\t')[:-1])
 
     terms = [_line_to_terms(line) for line in table]
+    subjects = _find_subjects(_terms_to_tree(terms))
 
-    for term in terms:
-        if term.parent == 0:
-            root = term
-            break
-
-    node = _make_tree(root, terms)
-
-    things = []
-    _crawl_tree(node, things)
-    subjects = []
-    for thing in things:
-        descriptors = sorted(thing.descriptors, key=lambda a: a.id)
-        subject = thing.noun.word.lower()
+    formatted_subjects = []
+    for subject in subjects:
+        descriptors = sorted(subject.descriptors, key=lambda a: a.id)
+        subject = subject.noun.word.lower()
         if len(descriptors) > 0:
             subject = ' {}'.format(subject)
 
-        subjects.append('{}{}'.format(' '.join([descriptor.word.lower() for descriptor in descriptors]), subject))
+        formatted_subjects.append('{}{}'.format(' '.join([descriptor.word.lower() for descriptor in descriptors]), subject))
 
-    return subjects
+    return formatted_subjects
+
+print list_subjects('Everything must be made of thin, strong paper without cuts or glue')
